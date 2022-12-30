d800f09a7f8ab7a4bd5440ca4ac8bf6a04ccfbf3
krunal <krunal.maru1106@gmail.com>
2022-12-30 15:00:12 +0530

    login

--- Eshop/shop/urls.py
+++ Eshop/shop/urls.py
@@ -5,4 +5,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.home, name='home'),
     path('signup/', views.signup,name='signup'),
+    path('login', views.loginuser, name='login'),
+
 ] +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Eshop/shop/views.py
+++ Eshop/shop/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,HttpResponse,redirect
 from .models import Product,Category,Customer
+from django.contrib.auth.hashers import check_password,make_password
 # Create your views here.
+
 def home(request):
     categories = Category.get_all_categories()
     categoryID = request.GET.get('category')
@@ -11,47 +13,77 @@ def home(request):
     context = {'product':product,'categories':categories}
     return render(request, 'shop/home.html',context)
 
+def validateCustomer(customer):
+    error_message = None
+    if (not customer.first_name):
+            error_message = 'First Name required'
+    elif(len(customer.first_name)<3):
+        error_message = 'First Name Should be 3 char required '
+    elif(not customer.last_name):
+        error_message = 'Last Name required'
+    elif len(customer.last_name) < 4:
+        error_message = 'Last Name Should be 4 char required'
+    elif(not customer.phone):
+        error_message = 'phone Number required'
+    elif len(customer.phone) < 10:
+        error_message = 'Phone Must be 10 Char'
+    elif(not customer.password):
+        error_message = 'Password Must required'
+    elif customer.isExists():
+        error_message = 'Customer Already Exist'    
+    return error_message
+
+def registeruser(request):
+    firstname = request.POST.get('first_name')
+    lastname = request.POST.get('last_name')
+    email = request.POST.get('email')
+    password = request.POST.get('password')
+    phone = request.POST.get('phone')
+    
+    value = {'firstname':firstname,'lastname':lastname,'email':email,'phone':phone}
+
+    error_message = None
+
+    customer = Customer(first_name=firstname,last_name=lastname,email=email,password=password,phone=phone)
+    error_message = validateCustomer(customer)
+
+
+    if  not error_message:
+        customer.password = make_password(customer.password)
+        customer.register()
+        return redirect('home')
+    else:
+
+        data = {
+            'error':error_message,
+            'values':value
+        }
+        return render(request,'shop/signup.html', data)
+
+
 def signup(request):
-    if request.method == 'POST':
-        firstname = request.POST.get('first_name')
-        lastname = request.POST.get('last_name')
+    if request.method == 'GET':
+        return render(request, 'shop/signup.html')            
+    else:
+        return registeruser(request)
+
+def loginuser(request):
+    if request.method == 'GET':
+        return render(request,'shop/login.html')
+    else:
         email = request.POST.get('email')
         password = request.POST.get('password')
-        phone = request.POST.get('phone')
-
-        value = {'firstname':firstname,'lastname':lastname,'email':email,'phone':phone}
-
+        customer = Customer.get_customer_by_email(email)
         error_message = None
-
-        customer = Customer(first_name=firstname,last_name=lastname,email=email,password=password,phone=phone)
-
-
-        if (not firstname):
-            error_message = 'First Name required'
-        elif(len(firstname)<3):
-            error_message = 'First Name Should be 3 char required '
-        elif(not lastname):
-            error_message = 'Last Name required'
-        elif len(lastname) < 4:
-            error_message = 'Last Name Should be 4 char required'
-        elif(not phone):
-            error_message = 'phone Number required'
-        elif len(phone) < 10:
-            error_message = 'Phone Must be 10 Char'
-        elif(not password):
-            error_message = 'Password Must required'
-        elif customer.isExists():
-            error_message = 'Customer Already Exist'    
-
-        if  not error_message:
-            customer.register()
-            return redirect('home')
+        if customer:
+            flag =  check_password(password,customer.password)
+            if flag:
+                return redirect('home')
+            else:
+                error_message = 'Email or Password Invalid'
         else:
+            error_message = 'Invalid creadential s!!'    
 
-            data = {
-                'error':error_message,
-                'values':value
-            }
-            return render(request,'shop/signup.html', data)
-    else:
-        return render(request, 'shop/signup.html')    
+        print(customer)
+        print(email,password)
+        return render(request, 'shop/login.html',{'error':error_message})
